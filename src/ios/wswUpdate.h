e27d81c34c4fe045cd27487ebfb65d0c04d04347
MaXiaosuai <1002547276@qq.com>
2022-04-22 10:59:00 +0800

    Add files via upload

--- src/ios/wswUpdate.h
+++ src/ios/wswUpdate.h
@@ -1,32 +1,8 @@
-#import "wswUpdate.h"
+#import <Cordova/CDV.h>
 
+@interface wswUpdate:CDVPlugin
 
-@implementation wswUpdate
-
-- (void)wswUpdate:(CDVInvokedUrlCommand *)command
-{
-    NSDictionary *params = [command.arguments objectAtIndex:0];
-       if (![params objectForKey:@"url"]) {
-           [self failWithCallbackID:command.callbackId withMessage:@"参数格式错误"];
-           return ;
-       }
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:params[@"url"]] options:@{} completionHandler:nil];
-}
-
-
-- (void)successWithCallbackID:(NSString *)callbackID withMessage:(NSString *)message
-{
-    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
-    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
-}
-
-
-
-- (void)failWithCallbackID:(NSString *)callbackID withMessage:(NSString *)message
-{
-    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:message];
-    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
-}
-
+- (void)wswUpdate:(CDVInvokedUrlCommand *)command;
 
 @end
+
